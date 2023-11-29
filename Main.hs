261e3537d2f7ac3777aa1a62843e0e0413005c45
jodonpacc <jodonne1@trinity.edu>
2023-11-29 12:21:25 -0600

    updated main to support -w flag

--- Main.hs
+++ Main.hs
@@ -13,10 +13,10 @@ import Chopsticks
 import System.Environment
 import System.IO
 
-data Flag = Help | Winner | Depth String deriving(Eq, Show)
+data Flag = Help | WinFlag | Depth String deriving(Eq, Show)
 options :: [OptDescr Flag]
 options = [ Option ['h'] ["help"] (NoArg Help) "Print usage information and exit.",
-            Option ['w'] ["winner"] (NoArg Winner) "Prints out the best move with no cut-off depth",
+            Option ['w'] ["winner"] (NoArg WinFlag) "Prints out the best move with no cut-off depth",
             Option ['d'] ["depth"] (ReqArg Depth "<num>") "Allows user to specify <num> as a cutoff depth"
           ]
 main :: IO ()
@@ -24,10 +24,11 @@ main = do
     args <- getArgs
     let (flags, inputs, errors) =  getOpt Permute options args
     let fileName = if null inputs then "Game_Log.txt" else head inputs
-    if Help`elem` flags
-      then putStrLn $ usageInfo "Chopsitcks [options] [file]" options
-      else
-            do game <- loadGame fileName
-            putBestMove game
-
-
+    if Help `elem` flags
+      then putStrLn $ usageInfo "Chopsticks [options] [file]" options
+      else if WinFlag `elem` flags
+           then do game <- loadGame fileName
+                   putBestMove game
+           else putStrLn "" --if Depth `elem` flags
+             --then -- use (read Depth) as cut-off param for new bestMove
+             --else -- new bestMove with depth as 5
