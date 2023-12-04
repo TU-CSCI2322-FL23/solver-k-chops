a37b3f6eb5529edb985d9b649ce5f12e2310f44c
ashkaty <ashkaty@gmail.com>
2023-12-03 23:02:26 -0600

    rateGame finished

--- Solver.hs
+++ Solver.hs
@@ -50,3 +50,36 @@ bestMove game =
                     case lookup (Tie) outcomes of
                         Just move -> Just move
                         Nothing -> Nothing
+
+handDiff :: [Hand] -> [Hand] -> Int
+handDiff playerA playerB = (length playerA) - (length playerB) 
+--calculates the difference in hands between players. If the resulting Int is negative, than playerA has less hands than playerB. If resulting Int is positive, than playerA has more hands 
+--Ex: handDiff (playerOne game) (playerTwo game) where playerOne has [2,3] and playerTwo has [4,5,6] = -1 
+
+-- scoreDist :: [Hand] ->  Int
+-- scoreDist player = 
+--     let 
+--         totalFingers = sum player
+--         avgFingerPerHand = totalFingers `div` (length player)
+--         dist = [abs (x - avgFingerPerHand) | x <- player]
+--         -- scoredDist = map (* (-1)) dist --this basically says that every negative number is positive and every positive number is negative. This promotes players where their hands have less fingers than average. It'll mean players play more defensively 
+--     in
+--         sum dist
+
+scoreWinnerLoser :: Game -> Int
+scoreWinnerLoser game = 
+    let result = getResult game
+    in
+        case result of
+            Just (Winner PlayerOne) -> 2
+            Just (Winner PlayerTwo) -> -2
+            Just Tie -> 1
+            Nothing -> 0
+
+rateGame :: Game -> Int
+scoreGame game = 
+    let 
+        p1HandDiff = handDiff (playerOne game) (playerTwo game)
+        p1WinScore = scoreWinnerLoser game 
+    in
+        p1HandDiff + p1WinScore
